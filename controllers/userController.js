2f32d60297e5d6f4cb4e70224b50fdfab158f01f
ch33kaboo <mc.ouzane@esi-sba.dz>
2022-04-30 17:15:30 +0100

    added modify user controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,12 +1,14 @@
 const asyncHandler = require('express-async-handler')
 const User = require('../models/userModel')
+const mongoose = require('mongoose')
 
 const registerUser = asyncHandler( async(req, res) => {
     const {email, password, name} = req.body
 
+    res.status(400) //set status to 400 in case schema validation failed
     const user = await User.create({name, email, password})
 
-    res.json(user)
+    res.status(201).json(user)  //in case not failed, set status to 201, created
 })
 
 const loginUser = asyncHandler( async(req, res) => {
@@ -22,7 +24,43 @@ const loginUser = asyncHandler( async(req, res) => {
 })
 
 const updateUser = asyncHandler( async(req, res) => {
-    res.json({ message : `user ${req.params.id} updated` })
+
+    const {name, email, password} = req.body
+
+    //check that the ID is valid
+    if (! mongoose.Types.ObjectId.isValid(req.params)) {
+        res.status(400)
+        throw new Error('this id is not valid')
+    }
+
+    const user = await User.findById(req.params.id)
+
+    //if user doesnt exist
+    if (!user) {
+        res.status(400)
+        throw new Error('user doesnt exist')
+    }
+
+    //if he want to update name
+    if (name) {
+        user.name = name
+        await user.save()
+        res.json({ message : `user's name was updated` , user : user})
+    }
+
+    //if he want to update email
+    if (email) {
+        user.email = email
+        await user.save()
+        res.json({ message : `user's email was updated` , user : user})
+    }
+
+    //if he want to update password
+    if (password) {
+        user.password = password
+        await user.save()
+        res.json({ message : `user's password was updated` , user : user})
+    }
 })
 
 const deleteUser = asyncHandler( async(req, res) => {
